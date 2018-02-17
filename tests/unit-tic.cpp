edba598d19af4cb89b90cda690b00e8da811d81d
Erwan BERNARD <bernard.erwan@gmail.com>
2018-02-17 11:27:03 +0100

    remove section in tic testing and add tests for entranglement counter

--- tests/unit-tic.cpp
+++ tests/unit-tic.cpp
@@ -1,29 +1,51 @@
 #include "catch.hpp"
 #include "tools.hpp"
 
-TEST_CASE("counter testing", "[Tic]") {
+TEST_CASE("unique counter", "[Tic]") {
 
   eb::Tic counter;
-
   REQUIRE(counter.get() == 0);
 
-  SECTION("increment of one") {
+  counter.incr();
+  REQUIRE(counter.get() == 1);
+
+  counter.reset();
+  REQUIRE(counter.get() == 0);
+
+  for (int i = 0; i < 100; ++i) {
     counter.incr();
+  }
+  REQUIRE(counter.get() == 100);
+  counter.reset();
+  REQUIRE(counter.get() == 0);
+}
 
-    REQUIRE(counter.get() == 1);
-  }
-  SECTION("increment of 100") {
-    for (int i = 0; i < 100; ++i) {
-      counter.incr();
-    }
+TEST_CASE("multiple counter entanglement", "[Tic]") {
 
-    REQUIRE(counter.get() == 100);
+  eb::Tic counter1;
+  eb::Tic counter2;
+  REQUIRE(counter1.get() == 0);
+  counter1.incr();
+  REQUIRE(counter1.get() == 1);
+
+  REQUIRE(counter2.get() == 0);
+  counter2.incr();
+  REQUIRE(counter2.get() == 1);
+
+  counter1.reset();
+  REQUIRE(counter1.get() == 0);
+
+  REQUIRE(counter2.get() == 1);
+  counter2.reset();
+  REQUIRE(counter2.get() == 0);
+
+  for (int i = 0; i < 100; ++i) {
+    counter1.incr();
+    counter2.incr();
   }
-  SECTION("reset") {
-    for (int i = 0; i < 100; ++i) {
-      counter.incr();
-    }
-    counter.reset();
-    REQUIRE(counter.get() == 0);
-  }
+  REQUIRE(counter1.get() == 100);
+  REQUIRE(counter2.get() == 100);
+  counter1.reset();
+  REQUIRE(counter1.get() == 0);
+  REQUIRE(counter2.get() == 100);
 }
