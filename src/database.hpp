899f9d2a75aed0fbb01d7d92a76126fbb0300c8b
Erwan BERNARD <bernard.erwan@gmail.com>
2018-03-03 16:24:37 +0100

    clean code in database and monitoring

--- src/database.hpp
+++ src/database.hpp
@@ -26,60 +26,28 @@ using json = nlohmann::json;
 
 namespace rm {
 
+// =================================================================================================
 class Database {
 public:
   virtual void hset(const std::string &prefix, const std::string &key, const std::string &value) = 0;
 
   virtual const std::map<std::string, std::string> &hgetall(std::string prefix) = 0;
 
-  std::string buildPrefix(char letter, int i) {
-    std::ostringstream ss;
-    ss << letter << std::setw(this->prefixLength - 1) << std::setfill('0') << i;
-    return ss.str();
-  }
+  std::string buildPrefix(char letter, int i);
 
   int prefixLength = 4;
 };
 
+// =================================================================================================
 class RocksdbDatabase : public Database {
 public:
-  RocksdbDatabase(std::string path = "/tmp/redis-monitor", bool clean = false) : path(path) {
-    // Configure RocksDB
-    rocksdb::Options DBOptions;
-    DBOptions.IncreaseParallelism();
-    DBOptions.compression = rocksdb::kNoCompression;
-    DBOptions.prefix_extractor.reset(rocksdb::NewFixedPrefixTransform(this->prefixLength));
-    DBOptions.create_if_missing = true;
-
-    if (clean) {
-      rocksdb::DestroyDB(this->path, DBOptions);
-    }
-
-    rocksdb::Status s = rocksdb::DB::Open(DBOptions, this->path, &this->db);
-    if (!s.ok()) {
-      std::cout << s.ToString() << std::endl;
-    }
-  }
+  RocksdbDatabase(std::string path = "/tmp/redis-monitor", bool clean = false);
 
   ~RocksdbDatabase() { delete db; }
 
-  void hset(const std::string &prefix, const std::string &key, const std::string &value) {
-    this->db->Put(rocksdb::WriteOptions(), prefix + key, value);
-  }
+  void hset(const std::string &prefix, const std::string &key, const std::string &value);
 
-  const std::map<std::string, std::string> &hgetall(std::string prefix) {
-    auto iter = db->NewIterator(rocksdb::ReadOptions());
-    rocksdb::Slice rdb_prefix = prefix;
-
-    this->tmpData.clear();
-
-    for (iter->Seek(rdb_prefix); iter->Valid() && iter->key().starts_with(rdb_prefix); iter->Next()) {
-      std::string tmp = iter->key().ToString();
-      (this->tmpData)[tmp.substr(this->prefixLength, tmp.size())] = iter->value().ToString();
-    }
-
-    return this->tmpData;
-  }
+  const std::map<std::string, std::string> &hgetall(std::string prefix);
 
 private:
   std::string path;
@@ -87,20 +55,72 @@ private:
   std::map<std::string, std::string> tmpData;
 };
 
+// =================================================================================================
 class StlDatabase : public Database {
 public:
   StlDatabase() {}
-  void hset(const std::string &prefix, const std::string &key, const std::string &value) {
-    mx.lock();
-    this->data[prefix][key] = value;
-    mx.unlock();
-  }
+
+  void hset(const std::string &prefix, const std::string &key, const std::string &value);
+
   const std::map<std::string, std::string> &hgetall(std::string prefix) { return this->data[prefix]; }
 
 private:
   std::mutex mx;
   std::map<std::string, std::map<std::string, std::string>> data;
 };
+
+// =================================================================================================
+inline std::string Database::buildPrefix(char letter, int i) {
+  std::ostringstream ss;
+  ss << letter << std::setw(this->prefixLength - 1) << std::setfill('0') << i;
+  return ss.str();
+}
+
+// =================================================================================================
+inline RocksdbDatabase::RocksdbDatabase(std::string path, bool clean) : path(path) {
+  // Configure RocksDB
+  rocksdb::Options DBOptions;
+  DBOptions.IncreaseParallelism();
+  DBOptions.compression = rocksdb::kNoCompression;
+  DBOptions.prefix_extractor.reset(rocksdb::NewFixedPrefixTransform(this->prefixLength));
+  DBOptions.create_if_missing = true;
+
+  if (clean) {
+    rocksdb::DestroyDB(this->path, DBOptions);
+  }
+
+  rocksdb::Status s = rocksdb::DB::Open(DBOptions, this->path, &this->db);
+  if (!s.ok()) {
+    std::cout << s.ToString() << std::endl;
+  }
+}
+
+// =================================================================================================
+inline void RocksdbDatabase::hset(const std::string &prefix, const std::string &key, const std::string &value) {
+  this->db->Put(rocksdb::WriteOptions(), prefix + key, value);
+}
+
+// =================================================================================================
+inline const std::map<std::string, std::string> &RocksdbDatabase::hgetall(std::string prefix) {
+  auto iter = db->NewIterator(rocksdb::ReadOptions());
+  rocksdb::Slice rdb_prefix = prefix;
+
+  this->tmpData.clear();
+
+  for (iter->Seek(rdb_prefix); iter->Valid() && iter->key().starts_with(rdb_prefix); iter->Next()) {
+    std::string tmp = iter->key().ToString();
+    (this->tmpData)[tmp.substr(this->prefixLength, tmp.size())] = iter->value().ToString();
+  }
+
+  return this->tmpData;
+}
+
+// =================================================================================================
+inline void StlDatabase::hset(const std::string &prefix, const std::string &key, const std::string &value) {
+  mx.lock();
+  this->data[prefix][key] = value;
+  mx.unlock();
+}
 
 }; // namespace rm
 
--- src/monitor.hpp
+++ src/monitor.hpp
@@ -21,6 +21,7 @@
 
 namespace rm {
 
+// =================================================================================================
 class Tic {
 public:
   Tic(std::string name = "default") : name(name) {}
@@ -33,33 +34,19 @@ private:
   std::string name;
 };
 
+// =================================================================================================
 class Monitor {
 public:
   Monitor(Database &database, int prefix, char letter = 'a')
       : database(database), prefix(this->database.buildPrefix(letter, prefix)) {}
+
   // virtual void add(std::string data, int value) = 0;  // Differe in function of update policy
 
   void addSeparator(std::string date) { this->database.hset(this->prefix, date, "null"); }
 
   const std::map<std::string, std::string> &get() { return database.hgetall(this->prefix); }
 
-  nlohmann::json get_json(std::string id) {
-    nlohmann::json data;
-    nlohmann::json abscisse = json::array();
-    nlohmann::json ordinate = json::array();
-
-    auto tmp = this->get();
-    for (auto &&it = tmp.begin(); it != tmp.end(); it++) {
-      abscisse.push_back(it->first);
-      ordinate.push_back(it->second);
-    }
-
-    data["id"] = id;
-    data["abscisse"] = abscisse;
-    data["ordinate"] = ordinate;
-
-    return data;
-  }
+  nlohmann::json get_json(std::string id);
 
   std::pair<std::string, std::string> lastData;
 
@@ -69,14 +56,12 @@ protected:
   std::string prefix;
 };
 
+// =================================================================================================
 class MonitorLength : public Monitor {
 public:
   MonitorLength(Database &database, int prefix) : Monitor(database, prefix, 'm') {}
 
-  void add(std::string date, std::string keyLength) {
-    this->lastData = {date, keyLength};
-    this->database.hset(this->prefix, date, keyLength);
-  };
+  void add(std::string date, std::string keyLength);
 };
 
 class MonitorFrequency : public Monitor {
@@ -84,16 +69,45 @@ public:
   MonitorFrequency(Database &database, int prefix)
       : Monitor(database, prefix, 'f'), counter(this->database.buildPrefix('f', prefix)) {}
 
-  void incr() { this->counter.incr(); };
-  void add(std::string date) {
-    this->lastData = {date, std::to_string(this->counter.get())};
-    database.hset(this->prefix, date, std::to_string(this->counter.get()));
-    this->counter.reset();
-  };
+  void incr() { this->counter.incr(); }
+
+  void add(std::string date);
 
 private:
   Tic counter;
 };
+
+// =================================================================================================
+inline nlohmann::json Monitor::get_json(std::string id) {
+  nlohmann::json data;
+  nlohmann::json abscisse = json::array();
+  nlohmann::json ordinate = json::array();
+
+  auto tmp = this->get();
+  for (auto &&it = tmp.begin(); it != tmp.end(); it++) {
+    abscisse.push_back(it->first);
+    ordinate.push_back(it->second);
+  }
+
+  data["id"] = id;
+  data["abscisse"] = abscisse;
+  data["ordinate"] = ordinate;
+
+  return data;
+}
+
+// =================================================================================================
+inline void MonitorLength::add(std::string date, std::string keyLength) {
+  this->lastData = {date, keyLength};
+  this->database.hset(this->prefix, date, keyLength);
+}
+
+// =================================================================================================
+inline void MonitorFrequency::add(std::string date) {
+  this->lastData = {date, std::to_string(this->counter.get())};
+  database.hset(this->prefix, date, std::to_string(this->counter.get()));
+  this->counter.reset();
+}
 
 }; // namespace rm
 
